ba8bedca2e3eebb36aa832ae31a778972fea9acb
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-12-15 11:46:45 -0300

    implementation of heal

--- s04_projeto/app.js
+++ s04_projeto/app.js
@@ -16,14 +16,22 @@ new Vue({
       this.playerLife = 100;
       this.monsterLife = 100;
     },
-    attack(special = false) {
+    attack(special) {
       this.hurt("monsterLife", 5, 10, special);
-      this.hurt("playerLife", 5, 12, special);
+      this.hurt("playerLife", 5, 12, false);
     },
     hurt(prop, min, max, special) {
       const plus = special ? 5 : 0;
       const hurt = this.getRandom(min + plus, max + plus);
       this[prop] = Math.max(this[prop] - hurt, 0);
+    },
+    healAndHurt() {
+      this.heal(10, 15);
+      this.hurt("playerLife", 7, 12, false);
+    },
+    heal(min, max) {
+      const heal = this.getRandom(min, max);
+      this.playerLife = Math.min(this.playerLife + heal, 100);
     },
     getRandom(min, max) {
       const value = Math.random() * (max - min) + min;
