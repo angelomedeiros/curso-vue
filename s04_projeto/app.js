b0b97a57df50b6f69eae72a0f72773dd1318d00b
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-12-15 11:38:17 -0300

    implementation of attack

--- s04_projeto/app.js
+++ s04_projeto/app.js
@@ -15,7 +15,24 @@ new Vue({
       this.running = true;
       this.playerLife = 100;
       this.monsterLife = 100;
+    },
+    attack(special = false) {
+      this.hurt("monsterLife", 5, 10, special);
+      this.hurt("playerLife", 5, 12, special);
+    },
+    hurt(prop, min, max, special) {
+      const plus = special ? 5 : 0;
+      const hurt = this.getRandom(min + plus, max + plus);
+      this[prop] = Math.max(this[prop] - hurt, 0);
+    },
+    getRandom(min, max) {
+      const value = Math.random() * (max - min) + min;
+      return Math.round(value);
     }
   },
-  watch: {}
+  watch: {
+    hasResult(value) {
+      if (value) this.running = false;
+    }
+  }
 });
