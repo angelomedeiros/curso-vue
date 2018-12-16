b06155c268e83587fd859fcb1ad5e4adff5b0ab1
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-12-16 11:52:31 -0300

    brincando com o aruivo .vue

--- s06/vue-cli/projeto-simples/src/App.vue
+++ s06/vue-cli/projeto-simples/src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>{{ titulo }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      titulo: "Primeiro Componente com o VueJs"
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  background-color: gold;
+}
+
+h1 {
+  background-color: blueviolet;
+  color: #fff;
+  margin: 0;
+  padding: 10px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
+
+
+
