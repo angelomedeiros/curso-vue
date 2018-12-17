16aba1cb44e2009f1eb2d211f52f77930207e423
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-12-17 13:13:27 -0300

    working with components

---
+++ s07/componente-intro/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Counter</h1>
+    <counter/>
+    <counter/>
+    <counter/>
+    <counter/>
+    <counter/>
+  </div>
+</template>
+
+<script>
+import counter from "./components/Counter.vue";
+
+export default {
+  components: {
+    counter
+  }
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ s07/componente-intro/src/components/Counter.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <span>{{ counter }}</span>
+    <div class="buttons">
+      <button @click="counter++">increase</button>
+      <button @click="counter--">decrease</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      counter: 0
+    };
+  }
+};
+</script>
+
+<style>
+.buttons,
+span {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+}
+
+span {
+  border-bottom: 1px solid #ccc;
+  height: 30px;
+  padding: 0px 25px;
+}
+
+button {
+  height: 30px;
+  width: 150px;
+  border-radius: 15px;
+  color: aliceblue;
+  margin-left: 10px;
+  outline: none;
+  border: none;
+}
+
+.buttons button:first-child {
+  background-color: purple;
+}
+
+.buttons button:last-child {
+  background-color: orangered;
+}
+</style>
+
+
