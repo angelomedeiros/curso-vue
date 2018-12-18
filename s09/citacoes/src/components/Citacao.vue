31ef09259c5b4fe246de8034818329fa67019c0a
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-12-18 17:20:52 -0300

    Working with slots

---
+++ s09/citacoes/src/components/Citacao.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="citacao">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
+</style>
